b4a59973f460bc7fd3177447bf2c6cff40c9b594
a1k0u <alko7enko@gmail.com>
2022-05-11 23:51:05 +0300

    cpu task

---
+++ cpu-info.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cpu_info=(
+  "Model name" "CPU(s)" "CPU MHz"
+  "L1d cache" "L1i cache" "L2 cache"
+  "L3 cache" "Architecture"
+)
+
+file_cpu_info="cpu.txt"
+touch $file_cpu_info
+
+for element in "${cpu_info[@]}"
+do
+  command="$(lscpu | grep "$element" --max-count=1)"
+  echo "$command" >> $file_cpu_info
+done
